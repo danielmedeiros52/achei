95875d40d39469da1b0689a43175f8b889e0ac3b
Daniel Medeiros <danielmedeiros52@gmail.com>
2017-09-26 11:03:16 -0300

    correcao de conflitos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
-<<<<<<< HEAD
-import { CabecalhoComponent } from './cabecalho/cabecalho.component';
-import { MenuLateralComponent } from './menu-lateral/menu-lateral.component';
 
-=======
+import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MdButtonModule,MdCardModule,MdMenuModule,MdToolbarModule,MdIconModule} from '@angular/material';
->>>>>>> 929284f50c71ffa71f9e194005c641d4bafa4bdb
+import {CabecalhoComponent} from './cabecalho/cabecalho.component';
+import {MenuLateralComponent} from './menu-lateral/menu-lateral.component';
 @NgModule({
   declarations: [
     AppComponent,
